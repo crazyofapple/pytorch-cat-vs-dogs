8a6df7a0886ea312d23df408a0d1a6aa79afa9e4
crazyofapple <630450972@qq.com>
2017-05-07 15:11:35 +0800

    learn
    learn from the author's work

---
+++ data_utils.py
@@ -0,0 +1,36 @@
+#encoding:utf-8
+import torch
+import torch.utils.data as data
+import os
+from PIL import Image
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += n * val
+        self.count += n
+        self.avg = self.sum / self.count
+class TestImageFolder(data.Dataset):
+    def __init__(self, root, transform=None):
+        images = []
+        for filename in os.listdir(root):
+            if filename.endswith('jpg'):
+                images.append('{}'.format(filename))
+        self.root = root
+        self.imgs = images
+        self.transform = transform
+    def __getitem__(self, index):
+        filename = self.imgs[index]
+        img = Image.open(os.path.join(self.root, filename))
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, filename
+    def __len__(self):
+        return len(self.imgs)
+
